5acdf79a53946fe4134d3962f9e341ab97831166
camas <camas@users.noreply.github.com>
2023-02-17 20:01:45 +0000

    base url

--- snippets/gui-458892a867b8c12f/js/utils.js
+++
@@ -1,9 +0,0 @@
-export function define_css(name, value) {
-    var sheet = document.getElementById(name);
-    if (sheet == null) {
-        sheet = document.createElement("style");
-        sheet.id = name;
-        document.body.appendChild(sheet);
-    }
-    sheet.innerHTML = value;
-}
